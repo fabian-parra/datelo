9f9464aa00b2fe7b0534620b279cb211ded549e4
Fabian Parra <fabian.parram@usach.cl>
2017-09-02 22:17:09 -0300

    Initial configuration for webpack and react.

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import {render} from 'react-dom'
+import React from 'react'
+
+render(<h1>Hello world</h1>, document.getElementById('appContainer'))
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'main.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'test'),
+      path.resolve(__dirname, 'node_modules')
+    ],
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env'],
+            plugins: [
+              require('babel-plugin-transform-object-assign'),
+              require('babel-plugin-transform-object-rest-spread'),
+              require('babel-plugin-transform-async-to-generator'),
+              require('babel-plugin-transform-class-properties'),
+              require('babel-plugin-transform-flow-strip-types')
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    host: '0.0.0.0',
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.template.html')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
