c2815a199d185c5e004db6e8ef8e2e6edb2b19d0
Fabian Parra <fabian.parram@usach.cl>
2017-10-15 18:25:17 -0300

    Adding suport for enzyme and chai-enzyme for testing.

--- test/unit/appTest.js
+++ test/unit/appTest.js
@@ -1,7 +1,16 @@
+import React from 'react'
 import {expect} from 'chai'
+import {shallow} from 'enzyme'
+import App from 'app'
 
 describe('App', () => {
+  const app = shallow(<App/>)
+
   it('renders header component', () => {
-    expect(true).to.be.true
+    expect(app.find('Header')).to.be.exist
+  })
+
+  it('renders footer component', () => {
+    expect(app.find('Footer')).to.be.exist
   })
 })
